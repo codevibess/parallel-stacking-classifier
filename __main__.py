b111634c5fcedadc10f7768610f6728683e11b2e
brzegorz <bogdal.grzegorz@tutanota.com>
2019-12-28 13:08:39 +0100

    Refactor

--- __main__.py
+++ __main__.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import seaborn as sn
-# Import datasets, classifiers and performance metrics
+import time
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
-
+from sklearn.metrics import accuracy_score
 from MultiClassifier import MultiClassifier
+import timeit
+
+num_cores = 1
+num_repetitions = 10
 
 def load_mnist_train_test():
     # The digits dataset
@@ -20,34 +24,26 @@ def load_mnist_train_test():
     
     # Split data into train and test subsets
     X_train, X_test, y_train, y_test = train_test_split(
-        data, digits.target, test_size=0.2, shuffle=False)
+        data, digits.target, test_size=0.3, shuffle=False)
     return X_train, X_test, y_train, y_test
 
+def print_results(description, accuracy, time, num_cores):
+    print(description)
+    print("Accuracy: " + str(accuracy), ", Time: " + str(time) + " seconds" + ", cores: " + str(num_cores))
 
-
+def train_test_run(X_train, X_test, y_train, y_test, num_cores):
+    start = time.time()
+    clf1 = svm.SVC(gamma=0.001)
+    clf2 = RandomForestClassifier()
+    clf3 = KNeighborsClassifier()
+    clf4 = GaussianNB()
+    classifier = MultiClassifier([clf1, clf2, clf3, clf4])
+    classifier.fit(X_train, y_train)
+    predicted = classifier.predict(X_test)
+    acc = accuracy_score(predicted, y_test)
+    end = time.time()
+    print_results("Train-test MNIST", acc, end - start, num_cores)
 
 X_train, X_test, y_train, y_test = load_mnist_train_test()
+train_test_run(X_train, X_test, y_train, y_test, num_cores)
 
-# Create a clf: a support vector clf
-clf1 = svm.SVC(gamma=0.001)
-clf2 = RandomForestClassifier()
-clf3 = KNeighborsClassifier()
-clf4 = GaussianNB()
-
-classifier = MultiClassifier([clf1, clf2, clf3, clf4])
-
-
-
-# We learn the digits on the first half of the digits
-classifier.fit(X_train, y_train)
-
-# Now predict the value of the digit on the second half:
-predicted = classifier.predict(X_test)
-
-data = {'y_Actual':    y_test,
-        'y_Predicted': predicted
-        }
-df = pd.DataFrame(data, columns=['y_Actual','y_Predicted'])
-confusion_matrix = pd.crosstab(df['y_Actual'], df['y_Predicted'], rownames=['Actual'], colnames=['Predicted'])
-print(confusion_matrix)
-sn.heatmap(confusion_matrix, annot=True)
