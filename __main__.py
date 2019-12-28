5700e64e61f9dc90311e82ad42e92e110702079a
vi be <vibe@vis-MacBook-Pro.local>
2019-12-28 12:37:58 +0200

    added sample data and bcasted it for all processes

--- __main__.py
+++ __main__.py
@@ -1,16 +1,35 @@
 from mpi4py import MPI
+import numpy as np
 
+# initialize MPI environment
 comm = MPI.COMM_WORLD
+size = comm.Get_size()
 rank = comm.Get_rank()
-print(rank)
-# if rank == 0:
-#     data = {'a': 7, 'b': 3.14}
-#     req = comm.isend(data, dest=1, tag=11)
-#     print('sended')
-#     req.wait()
-#     print('wait')
-# elif rank == 1:
-#     req = comm.irecv(source=0, tag=11)
-#     print('wait from rank 1')
-#     data = req.wait()
-#     print(data)
+
+if rank==0:
+    print(f"[INFO] Program runned in {size} processes")
+
+print(f"[INFO] Hello from process number {rank}")
+
+
+if rank == 0:
+    data = np.arange(100, dtype='i')
+    for i in range(100):
+        assert data[i] == i
+else:
+    data = np.empty(100, dtype='i')
+    pass
+
+
+
+print(f'[INFO] Bcasting data from the root process ({rank})') if rank == 0 else None
+bcast_start_time = MPI.Wtime()
+comm.Bcast(data, root=0)
+bcast_finish_time = MPI.Wtime()
+
+bcast_time = bcast_finish_time - bcast_start_time
+print(f'[TIME] Master process ({rank}) finished Bcasting data with time {bcast_time}') if rank == 0 else print(f'[TIME] Process {rank} finished receive bcasted data with time {bcast_time}')
+
+
+# MPI environment finalization
+MPI.Finalize()
