b3a49f74c2554a618832aa1c227972eb20f40c89
brzegorz <bogdal.grzegorz@tutanota.com>
2019-12-28 12:57:06 +0100

    Working MultiClassifier

--- __main__.py
+++ __main__.py
@@ -1,16 +1,53 @@
-from mpi4py import MPI
+import pandas as pd
+import seaborn as sn
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-print(rank)
-# if rank == 0:
-#     data = {'a': 7, 'b': 3.14}
-#     req = comm.isend(data, dest=1, tag=11)
-#     print('sended')
-#     req.wait()
-#     print('wait')
-# elif rank == 1:
-#     req = comm.irecv(source=0, tag=11)
-#     print('wait from rank 1')
-#     data = req.wait()
-#     print(data)
+from MultiClassifier import MultiClassifier
+
+def load_mnist_train_test():
+    # The digits dataset
+    digits = datasets.load_digits()
+    
+    # To apply a classifier on this data, we need to flatten the image, to
+    # turn the data in a (samples, feature) matrix:
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+    
+    # Split data into train and test subsets
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, digits.target, test_size=0.2, shuffle=False)
+    return X_train, X_test, y_train, y_test
+
+
+
+
+X_train, X_test, y_train, y_test = load_mnist_train_test()
+
+# Create a clf: a support vector clf
+clf1 = svm.SVC(gamma=0.001)
+clf2 = RandomForestClassifier()
+clf3 = KNeighborsClassifier()
+clf4 = GaussianNB()
+
+classifier = MultiClassifier([clf1, clf2, clf3, clf4])
+
+
+
+# We learn the digits on the first half of the digits
+classifier.fit(X_train, y_train)
+
+# Now predict the value of the digit on the second half:
+predicted = classifier.predict(X_test)
+
+data = {'y_Actual':    y_test,
+        'y_Predicted': predicted
+        }
+df = pd.DataFrame(data, columns=['y_Actual','y_Predicted'])
+confusion_matrix = pd.crosstab(df['y_Actual'], df['y_Predicted'], rownames=['Actual'], colnames=['Predicted'])
+print(confusion_matrix)
+sn.heatmap(confusion_matrix, annot=True)
