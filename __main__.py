1cc187fe664be76d1099a4e984602dea370b6161
brzegorz <bogdal.grzegorz@tutanota.com>
2020-01-03 15:19:40 +0100

    Bugfixes

--- __main__.py
+++ __main__.py
@@ -4,8 +4,10 @@ import time
 import numpy as np
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model.ridge import RidgeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from MultiClassifier import MultiClassifier
@@ -19,7 +21,7 @@ num_repetitions = 10
 def load_mnist_data():
     # The digits dataset
     digits = datasets.load_digits()
-    
+
     # To apply a classifier on this data, we need to flatten the image, to
     # turn the data in a (samples, feature) matrix:
     n_samples = len(digits.images)
@@ -30,19 +32,19 @@ def load_cifar10_data():
     data = list()
     labels = list()
     for batch_index in range(1, 6):
-        batch = unpickle("cifar-10-batches-py/data_batch_" + str(batch_index)) 
+        batch = unpickle("cifar-10-batches-py/data_batch_" + str(batch_index))
         data = data + list(batch[b"data"])
         labels = labels + batch[b"labels"]
-    return data, labels
-    
+    return np.array(data), np.array(labels)
+
 def load_cifar100_data():
     data = list()
     labels = list()
     for batch_name in ["test", "train"]:
-        batch = unpickle("cifar-100/" + batch_name) 
+        batch = unpickle("cifar-100/" + batch_name)
         data = data + list(batch[b"data"])
         labels = labels + batch[b"coarse_labels"]
-    return data, labels
+    return np.array(data), np.array(labels)
 
 def unpickle(file):
     import pickle
@@ -50,28 +52,33 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-def load_letter_data()
+def load_letter_data():
     data = pd.read_csv("letter-recognition/letter-recognition.data", header=None)
-    y = np.array(data.iloc[:,0])  
-    X = np.array(data.iloc[:,1:len(data)])  
-    return X, y 
+    y = np.array([ord(x) - 65 for x in data.iloc[:,0]])                                                  
+    X = np.array(data.iloc[:,1:len(data)])
+    return X, y
 
 def print_results(description, accuracy, time, num_cores):
     print(description)
     print("Accuracy: " + str(accuracy), ", Time: " + str(time) + " seconds" + ", cores: " + str(num_cores))
 
 def get_multi_classifier():
-    clf1 = svm.SVC(gamma=0.001)
-    clf2 = RandomForestClassifier()
-    clf3 = KNeighborsClassifier()
+    clf1 = RidgeClassifier()
+    clf2 = RandomForestClassifier(n_estimators = 10)
+    clf3 = LinearDiscriminantAnalysis()
     clf4 = GaussianNB()
-    classifier = MultiClassifier([clf1, clf2, clf3, clf4])
+    classifier = MultiClassifier([
+        clf1,
+        clf2,
+        clf3,
+        clf4
+        ])
     return classifier
 
 def train_test_run(X, y, num_cores, description):
+    print("Train-test run of " + description)
     # Split data into train and test subsets
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=False)
     start = time.time()
     classifier = get_multi_classifier()
     classifier.fit(X_train, y_train)
@@ -81,6 +88,7 @@ def train_test_run(X, y, num_cores, description):
     print_results(description, acc, end - start, num_cores)
 
 def cross_validation_run(X, y, num_cores, description):
+    print("CV run of " + description)
     start = time.time()
     kf = KFold(n_splits=10, shuffle = True)
     kf.get_n_splits(X)
@@ -95,8 +103,8 @@ def cross_validation_run(X, y, num_cores, description):
         accuracies.append(acc)
     acc = np.mean(accuracies)
     end = time.time()
-    print_results(description, acc, end - start, num_cores)    
-        
+    print_results(description, acc, end - start, num_cores)
+
 X_mnist, y_mnist = load_mnist_data()
 X_cifar10, y_cifar10 = load_cifar10_data()
 X_cifar100, y_cifar100 = load_cifar100_data()
